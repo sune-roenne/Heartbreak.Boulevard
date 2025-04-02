125b50b68aeda49353240d8a66ca8e933c76e1df
Sune Rønne <SURN@nykredit.dk>
2025-04-03 00:18:27 +0200

    For now

--- Heartbreak.Boulevard.UI/Components/SpotifyPlayerComponent.razor.cs
+++ Heartbreak.Boulevard.UI/Components/SpotifyPlayerComponent.razor.cs
@@ -6,23 +6,11 @@ namespace Heartbreak.Boulevard.UI.Components;
 
 public partial class SpotifyPlayerComponent : IDisposable
 {
-    private static long _currentId = 0L;
-    private static readonly object _idLock = new { };
-    private static long NextId()
-    {
-        lock(_idLock)
-            return _currentId++;
-    }
-    private readonly long _id = NextId();
-
     private string PlayerId => $"hbb-spotify-player-div";
     private bool _didInitPlayer = false;
 
     [Inject]
     public IJSRuntime JS { get; set; }
-
-    [Parameter]
-    public string PlaylistId { get; set; }
 
     [Parameter]
     public TimeSpan? InitDelay { get; set; }
@@ -37,13 +25,21 @@ public partial class SpotifyPlayerComponent : IDisposable
     public HBBCommunication Communications { get; set; }
 
     private bool _registeredForCommunications = false;
+    private string? _currentPlaylistId = null;
 
-    protected override void OnParametersSet()
+
+
+    protected override async Task OnParametersSetAsync()
     {
         if(!_registeredForCommunications)
         {
             Communications.OnRadioIsEjectedChanged += OnRadioIsEjectedChanged;
             _registeredForCommunications = true;
+        }
+        if(Communications.CurrentPlaylistId != _currentPlaylistId)
+        {
+            await JS.InvokeVoidAsync("setPlaylistId", Communications.CurrentPlaylistId);
+            _currentPlaylistId = Communications.CurrentPlaylistId;
         }
     }
 
@@ -53,7 +49,7 @@ public partial class SpotifyPlayerComponent : IDisposable
         if(!_didInitPlayer)
         {
             _didInitPlayer = true;
-            await JS.InvokeVoidAsync("initSpotifyPlayer", PlayerId, PlaylistId);
+            await JS.InvokeVoidAsync("initSpotifyPlayer", PlayerId);
             await JS.InvokeVoidAsync("setDimensions", ExpandedWidth, Height);
 
         }
@@ -76,6 +72,13 @@ public partial class SpotifyPlayerComponent : IDisposable
     public void Dispose()
     {
         if(_registeredForCommunications)
+        {
             Communications.OnRadioIsEjectedChanged -= OnRadioIsEjectedChanged;
+            _registeredForCommunications = false;
+        }
     }
+
+    private record CurrentlyPlayTrackInfo(string? PlaylistId, string TrackName);
+
+
 }
